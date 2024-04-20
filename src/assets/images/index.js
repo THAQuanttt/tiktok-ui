ba6202c3e89f7b90878756ea77746954371280ac
21120537 <145359313+THAQuanttt@users.noreply.github.com>
2024-04-20 16:55:41 +0700

    design header logo and button search

---
+++ src/assets/images/index.js
@@ -0,0 +1,5 @@
+const images = {
+    logo: require('~/assets/images/logo.svg'),
+    home: require('~/assets/images/house-solid.svg'),
+};
+export default images;
--- src/components/Layout/component/Header/index.js
+++ src/components/Layout/component/Header/index.js
@@ -1,10 +1,29 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import images from '~/assets/images';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 const cx = classNames.bind(styles);
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo.default} alt="tiktok"></img>
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos"></input>
+                    <button className={cx('search-clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark}></FontAwesomeIcon>
+                    </button>
+                    <FontAwesomeIcon className={cx('spiner')} icon={faSpinner}></FontAwesomeIcon>
+                    <span className={cx('border')}></span>
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
+            </div>
         </header>
     );
 }
